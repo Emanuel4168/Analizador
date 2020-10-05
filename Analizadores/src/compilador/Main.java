92a2d2908b2c42085c220d0e5b76784fcc835291
Emanuel4168 <emanuel.l4168@gmail.com>
2020-10-04 18:17:35 -0600

    Cargado el proyecto inicial

---
+++ Analizadores/src/compilador/Main.java
@@ -0,0 +1,15 @@
+package compilador;
+
+import Grafico.Ventana;
+
+/**
+ *
+ * @author Carlos Contreras
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Ventana();
+    }
+
+}
