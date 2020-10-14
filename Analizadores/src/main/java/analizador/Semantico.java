011142dab4930bfac2203e9ec938efcdf71dac9d
JuanEmus <emus75@hotmail.com>
2020-10-13 21:12:38 -0600

    Agregada validacion para variables no declaradas

--- Analizadores/src/main/java/analizador/Semantico.java
+++ Analizadores/src/main/java/analizador/Semantico.java
@@ -1,6 +1,9 @@
 package main.java.analizador;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Semantico {
 	private ArrayList<ValoresTabla> tablaSimbolos;
@@ -23,6 +26,7 @@ public class Semantico {
 	
 	public ArrayList<String> checkSemantic(){
 		CheckAssignments();
+		checkDuplicity();
 		return this.errors;
 	}
 	
@@ -36,6 +40,24 @@ public class Semantico {
 				errors.add("Error semantico en linea " +currentIdentifier.renglon +", identificador "+ currentIdentifier.nombre +" Se intenta asignar un valor entero a un boolean");
 			}
 		}
+		return true;
+	}
+	
+	public boolean checkDuplicity() {
+		String[] lines = code.split("\\r?\\n"); 
+		for(int i = 0; i < lines.length; i++) { 
+			if(lines[i].contains(" class ")) {
+				continue;
+			}
+			List<String> identifiers = this.getAllIdentifiers(lines[i]);
+			for(int j = 0; j < identifiers.size(); j++) {
+				String currentIdentifier = identifiers.get(j);
+				if(!this.existIdentifier(currentIdentifier,i)) {
+					errors.add("Error semantico en linea "+(i+1)+", el identificador "+ currentIdentifier + " no ha sido declarado");
+				}
+			}
+		}
+		
 		return true;
 	}
 	
@@ -53,6 +75,28 @@ public class Semantico {
 	    }
 
 	    return true;
+	}
+	
+	public List<String> getAllIdentifiers(String input) {
+	    final String regex = "[a-z]{1,40}[0-9]{1,40}";
+
+	    final Matcher m = Pattern.compile(regex).matcher(input);
+
+	    final List<String> matches = new ArrayList<>();
+	    while (m.find()) {
+	        matches.add(m.group(0));
+	    }
+
+	    return matches;
+	}
+	
+	private boolean existIdentifier(String name, int linea) {
+		for(ValoresTabla simbolo: this.tablaSimbolos) {
+			if(simbolo.nombre.equals(name) && Integer.parseInt(simbolo.renglon) <= (linea + 1)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
